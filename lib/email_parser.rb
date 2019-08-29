cf4e6210bfa99b5dee8c3b5a65cdeaad9d806412
Jeong Park <wjdtn1220@gmail.com>
2019-08-29 01:16:58 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,16 +4,4 @@
 # or whitespace (' ').
 class EmailAddressParser
   
-  attr_accessor :names, :csv_emails
-  
-  def initialize(csv_emails)
-    @csv_emails = csv_emails
-  end
-  
-  def parse
-    csv_emails.split.collect do |address|
-      address.split(",")
-    end
-    .flatten.uniq
-  end
 end
